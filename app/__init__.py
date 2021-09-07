83526e031ad265dc658b2c76e4dafe0ef3f8c0b6
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-07 18:50:31 +0300

    modified the page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+# from flask_bootsrap import Bootstrap
 #Initializing application
 app = Flask(__name__,instance_relative_config= True)
 
@@ -7,5 +9,11 @@ app = Flask(__name__,instance_relative_config= True)
 app .config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+#Initializing Flask Extentions
 
+bootstrap = Bootstrap(app)
+
+
+
+from app import error
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,25 +1,17 @@
 from flask import render_template
 from app import app
-from .request import get_movies
-
-# @app.route('/')
-# def index():
-#     '''
-#     View root page function that returns the 
-#     index page and its data
-#     '''
-#     message = 'Hello peter welcome to flask'
-#     return render_template('index.html',message=message)
+from .request import get_movies,get_movie
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
     '''
     View movie page function that returns the movie details page and its data
 
     '''
-    title = "Movie shop"
-    return render_template('movie.html',title = title,id = movie_id)
+    movie = get_movie(id)
+    title = f'{"Movie.title"}'
+    return render_template('movie.html',title=title,movie = movie)
 def index():
     '''
     View root page function that returns the index page and its data
@@ -30,15 +22,13 @@ def index():
     return render_template('index.html',title = title)
 
 @app.route('/')
-
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
