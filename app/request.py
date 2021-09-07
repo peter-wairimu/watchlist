21fd99a3ca0c70253384f10a90e9cb440c25ee0a
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-07 20:46:52 +0300

    added searchbar

--- app/request.py
+++ app/request.py
@@ -23,11 +23,30 @@ def get_movies(category):
         movie_results = None
 
         if get_movies_response['results']:
-            movie_results_list = get_movies_response['results']
             movie_results = process_results(movie_results_list)
 
 
     return movie_results
+
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+
+    return search_movie_results
+
+
+
+
+
 
 def process_results(movie_list):
     '''
