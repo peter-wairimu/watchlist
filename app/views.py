d6b0066d54423aa26cb1a43a62f97c04c5f37dea
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-07 22:05:20 +0300

    added forms module

--- app/views.py
+++ app/views.py
@@ -3,25 +3,8 @@ from app import app
 from .request import get_movies,get_movie,search_movie
 
 
-@app.route('/movie/<int:id>')
-def movie(id):
-    '''
-    View movie page function that returns the movie details page and its data
 
-    '''
-    movie = get_movie(id)
-    title = f'{"Movie.title"}'
-    return render_template('movie.html',title=title,movie = movie)
-def index():
-    '''
-    View root page function that returns the index page and its data
-
-    '''
-
-    title = 'Home - Welcome to the best Movie Review Website online'
-    return render_template('index.html',title = title)
-
-
+# Views
 @app.route('/')
 def index():
 
@@ -44,14 +27,24 @@ def index():
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
+
+
 @app.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
-
     '''
-
-    movie_name_list = movie_name.split("")
+    movie_name_list = movie_name.split(" ")
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
