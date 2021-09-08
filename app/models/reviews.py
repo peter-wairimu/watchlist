90b999e5afabf4716331cd75ec7a58894e41cb71
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-08 10:46:48 +0300

    modified config.py

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -13,3 +13,11 @@ class Review:
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear
+
+    @classmethod
+    def get_reviews(cls,id):
+        response = []
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+        return response
--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from .models import reviews
+
+Review = reviews.Review
 
 
 
@@ -35,8 +39,10 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+
 
 
 @app.route('/search/<movie_name>')
@@ -49,3 +55,23 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+
+    form = ReviewForm()
+
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
